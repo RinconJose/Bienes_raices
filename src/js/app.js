62ae35321a953d97333c3ce5b552bf303d5ab354
Jose Rincon <rincon.jmg@gmail.com>
2021-10-28 17:13:20 -0500

    creating menu responsive and adding its styles

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListener();
+});
+
+function eventListener() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle('mostrar');
+}
